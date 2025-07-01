44b5d45c16322c97807e7fd3f120120515cab985
KAVIYA303K <kaviyakarupusamy303@gmail.com>
2025-07-01 17:20:29 +0530

    Add files via upload

---
+++ README.py
@@ -0,0 +1,12 @@
+#  Password Generator 
+
+This is a simple password generator written in Python. It allows you to customize:
+- Password length
+- Inclusion of uppercase letters
+- Digits
+- Special characters
+
+#Features
+- Ensures strong password by including at least one character from each selected category
+- Randomized for high entropy
+- Easily customizable
